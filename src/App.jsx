28c38759645f437ebf13200449042b1da5c63641
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-06-08 11:51:15 +0330

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import Card from './components/Card'
 function App() {
 
   return (
-    <main className='bg-[url(/images/bg-top.svg),url(/images/bg-bottom.svg)] bg-no-repeat bg-cover min-h-screen max-w-[90rem] flex items-center justify-center mx-auto bg-light-gray font-mont'>
-      <div className='flex flex-col md:flex-row gap-6 md:gap-0'>
+    <main className='relative px-6 py-24 max-w-[90rem] flex items-center justify-center mx-auto bg-light-gray font-mont'>
+      <img src='/images/bg-bottom.svg' className='absolute bottom-0 left-0 z-0'/>
+      <img src='/images/bg-top.svg' className='absolute top-0 right-0 z-0'/>
+      <div className='flex flex-col md:flex-row gap-6 md:gap-0 z-10'>
       <Card
       text="Basic"
       title="199.99"
       text1="500 GB Storage"
       text2="2 Users Allowed"
       text3="Send up to 3 GB"
+      color4="#4A4D60"
       />
       <Card
       text="Professional"
@@ -23,7 +26,7 @@ function App() {
       color="#A2A7F0"
       color2="#FFFFFF"
       color3="#6D72DE"
-      className="md:h-[31.25rem] md:mt-[-1.2rem]"
+      className="md:h-[31.25rem] md:mt-[-1.7rem] md:py-4"
       />
       <Card
       text="Master"
@@ -31,6 +34,7 @@ function App() {
       text1="52 TB Storage"
       text2="10 Users Allowed"
       text3="Send up to 20 GB"
+      color4="#4A4D60"
       />
       </div>
     </main>
